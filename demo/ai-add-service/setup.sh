ba10ee78ddc63a53f42c0577605e5fbd983170a7
Michael Irwin <mikesir87@gmail.com>
2025-10-23 09:55:11 -0400

    Remove GHA workflows and demo folder (streamline the repo a bit)

--- demo/ai-add-service/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-echo "==> Applying patch and creating a commit"
-git apply --whitespace=fix "${SCRIPT_DIR}/demo.patch"
-git commit -am "Modify the compose.yaml to remove kafbat-ui"
